a6c0b56423f707fcae9cdf9821864ca98923c6a8
Amin Amirali <amin.amirali@telenordigital.com>
2018-11-09 12:54:13 +0100

    Add Adaboost example

---
+++ 3.6_ensemble_adaboost.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import AdaBoostClassifier
+model = AdaBoostClassifier()
+model.fit(x_train, y_train)
+model.predict(x_test)
+
+## Hyperparameters
+## base_estimator: The model utilized for the weak learners (Warning: Don't forget to import the model that you decide to use for the weak learner).
+## n_estimators: The maximum number of weak learners used.
