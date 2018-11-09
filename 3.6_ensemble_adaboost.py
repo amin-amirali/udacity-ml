b7ac52a0bb36474ff22ca4f346b9b362e6c931e7
Amin Amirali <amin.amirali@telenordigital.com>
2018-11-09 12:55:15 +0100

    Update Adaboost with usage example

--- 3.6_ensemble_adaboost.py
+++ 3.6_ensemble_adaboost.py
@@ -6,3 +6,8 @@ model.predict(x_test)
 ## Hyperparameters
 ## base_estimator: The model utilized for the weak learners (Warning: Don't forget to import the model that you decide to use for the weak learner).
 ## n_estimators: The maximum number of weak learners used.
+
+from sklearn.tree import DecisionTreeClassifier
+model = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth=2), n_estimators = 4)
+
+
